5f108daf504fddc971f4b1a2d5a5395faa07b667
hozaifa <hozaifahnzla1111@gmail.com>
2024-01-24 00:02:19 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+// Main.java
+        public static void main(String[] args) {
+            RWOnly obj = new RWOnly();
+
+            // Step 4: Try setting a value to name directly (will result in an error)
+            // Uncommenting the following line will result in a compilation error
+            // obj.name = "John";
+
+            // Try printing it (will also result in an error)
+            // Uncommenting the following line will result in a compilation error
+            // System.out.println(obj.name);
+
+            // Step 6: Set a value to name using setter function
+            obj.setName("John");
+
+            // Access its value using getter function
+            System.out.println(obj.getName()); // Output: John
+        }
+    }
+
   
-}
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class RWOnly {
-    
-}
+    // RWOnly.java
+
+        private String name;  // Task 2
+
+        // Task 5: Implementing getter and setter functions
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
